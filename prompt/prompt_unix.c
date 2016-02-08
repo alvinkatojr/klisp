79ac710ba2ac30fb769b8c59b307fda4a0d51009
Alvin Kato J.R <alvinkatojr@gmail.com>
2016-02-08 23:19:28 +0300

    Refactors prompt to use readline and add_history methods

--- prompt/prompt_unix.c
+++ prompt/prompt_unix.c
@@ -4,9 +4,6 @@
 #include <editline/readline.h>
 #include <editline/history.h>
 
-/* Declare a buffer for user input of size 2048 */
-static char input[2048];
-
 int main(int argc, char** argv){
 
   /* Print Version and Exit Information */
@@ -17,13 +14,16 @@ int main(int argc, char** argv){
   while (1){
 
     /* Output our prompt and get input */
-    fputs("lispy> ", stdout);
+    char* input = readline("klispy> ");
 
     /* Read a line of user input of maximum size 2048 */
-    fgets(input, 2048, stdin);
+    add_history(input);
 
     /* Echo input back to user */
     printf("No you're a %s\n", input);
+
+    /* Free retrieved input */
+    free(input);
   }
 
   return 0;
