18cff2f68b1678841c1af99cfe052ce11fad5079
Alvin Kato J.R <alvinkatojr@gmail.com>
2016-02-09 23:42:31 +0300

    Adds fake readline function

--- prompt/prompt_unix.c
+++ prompt/prompt_unix.c
@@ -5,7 +5,17 @@
 #ifdef _WIN32
 #include <string.h>
 
+static char buffer[2048];
 
+/* Fake readline function */
+char* readline(char* prompt){
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
 
 #include <editline/readline.h>
 
