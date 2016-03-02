1e7d438115a7f5666a943eeea3d2811c2051198f
Alvin Kato J.R <alvinkatojr@gmail.com>
2016-03-02 23:56:55 +0300

    Adds 4 new parsers

--- prompt/parsing_unix.c
+++ prompt/parsing_unix.c
@@ -29,6 +29,12 @@ void add_history(char* unused) (){}
 
 int main(int argc, char** argv){
 
+  /* New Parsers */
+  mpc_paraser_t* Number = mpc_new("number");
+  mpc_paraser_t* Operator = mpc_new("operator");
+  mpc_paraser_t* Expr = mpc_new("expr");
+  mpc_paraser_t* Lispy = mpc_new("lispy");
+
   /* Print Version and Exit Information */
   puts("Klisp Version 0.0.0.0.1");
   puts("Press Ctrl+c to Exit\n");
