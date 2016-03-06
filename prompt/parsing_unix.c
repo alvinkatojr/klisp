17cea2744d8a05020d725f3062d1d3afcefb0e64
Alvin Kato J.R <alvinkatojr@gmail.com>
2016-03-06 23:23:48 +0300

    Adds eval_op function

--- prompt/parsing_unix.c
+++ prompt/parsing_unix.c
@@ -26,6 +26,16 @@ void add_history(char* unused) (){}
 #include <editline/readline.h>
 #endif
 
+/* Use the operator string to see which operation to perform */
+
+long eval_op(long x, char* op, long y){
+    if (strcmp(op, "+") == 0) { return x + y; }
+    if (strcmp(op, "-") == 0) { return x - y; }
+    if (strcmp(op, "*") == 0) { return x * y; }
+    if (strcmp(op, "/") == 0) { return x / y; }
+    return 0;
+}
+
 long eval(mpc_ast_t* t) {
 
   /* If tagged as number return it directly */
