e0efc53e94484a5acee3187acd08c97e7b3eab3c
Alvin Kato J.R <alvinkatojr@gmail.com>
2016-03-10 06:42:31 +0300

    Fixes typo, and indentation

--- prompt/parsing_unix.c
+++ prompt/parsing_unix.c
@@ -62,10 +62,10 @@ lval lval_err(int x){
 
 void lval_print(lval v){
   switch (v.type) {
-    case LVAL_NUM: printf("%li", v.num);
-    break;
-
-    case LVAL_ERR;
+    case LVAL_NUM:
+      printf("%li", v.num);
+      break;
+    case LVAL_ERR:
       if (v.err == LERR_DIV_ZERO){
         printf("ErrorL Division By Zero!");
       }
