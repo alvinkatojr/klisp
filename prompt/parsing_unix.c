7e64fe00ae7a34802e1bbfd92249b111a280839e
Alvin Kato J.R <alvinkatojr@gmail.com>
2016-03-12 19:47:47 +0300

    Renames operator to symbol

--- prompt/parsing_unix.c
+++ prompt/parsing_unix.c
@@ -147,7 +147,7 @@ int main(int argc, char** argv){
   mpca_lang(MPCA_LANG_DEFAULT,
     "                                                 \
       number : /-?[0-9]+/;                            \
-      operator : '+' | '-' | '*' | '/';               \
+      symbol : '+' | '-' | '*' | '/';               \
       expr : <number> | '(' <operator> <expr>+ ')';   \
       lispy: /^/ <operator> <expr>+ /$/;              \
     ",
