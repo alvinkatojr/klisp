0d20e85b75163f7cd06f139cfeae73a90dc1c0e5
Alvin Kato J.R <alvinkatojr@gmail.com>
2016-03-31 23:37:58 +0300

    Adds rule defition for sexpressions

--- prompt/parsing_unix.c
+++ prompt/parsing_unix.c
@@ -171,7 +171,8 @@ int main(int argc, char **argv){
   mpca_lang(MPCA_LANG_DEFAULT,
     "                                                 \
       number : /-?[0-9]+/;                            \
-      symbol : '+' | '-' | '*' | '/';               \
+      symbol : '+' | '-' | '*' | '/';                 \
+      sexpr  : '(' <expr>* ')';                       \
       expr : <number> | '(' <operator> <expr>+ ')';   \
       lispy: /^/ <operator> <expr>+ /$/;              \
     ",
