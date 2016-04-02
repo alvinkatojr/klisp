20e03285f838e4c520534acf437354d79bffaa0e
Alvin Kato J.R <alvinkatojr@gmail.com>
2016-04-03 00:42:25 +0300

    Adds lval_del method to free up memory for the lval types

--- prompt/parsing_unix.c
+++ prompt/parsing_unix.c
@@ -102,6 +102,30 @@ lval *lval_sexpr(void) {
   return v;
 }
 
+void lval_del(lval *v) {
+
+  switch(v->type) {
+    // Do nothing special for number type
+    case LVAL_NUM: break;
+
+    // For Err or Sym free the string data
+    case LVAL_ERR: free(v->err); break;
+    case LVAL_SYM: free(v->sym); break;
+
+    // if Sexpr then delete all elements inside
+    case LVAL_SEXPR:
+      for (int i = 0; i < v->count; i++) {
+        lval_del(v->cell[i]);
+      }
+      // Also free the memory allocated to contain the pointers
+      free(v->cell);
+    break;
+  }
+
+  // Free the memory allocated for the "lval" struct itself
+  free(v);
+}
+
 // Print an "lval"
 
 void lval_print(lval v){
