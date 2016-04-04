3c100920c386a995d4f301185d506af5132cf36b
Alvin Kato J.R <alvinkatojr@gmail.com>
2016-04-05 01:20:06 +0300

    Adds an lval_read method

--- prompt/parsing_unix.c
+++ prompt/parsing_unix.c
@@ -160,6 +160,29 @@ lval *lval_read_num(mpc_ast_t *t) {
   return errno != ERANGE ? lval_num(x) : lval_err("inavlid number");
 }
 
+lval *lval_read(mpc_ast_t *t) {
+  //  If Symbol or Number return conversion to that type
+  if (strstr(t->tag, "number")) { return lval_read_num(t); }
+  if (strstr(t->tag, "symbol")) { return lval_sym(t->contents); }
+
+  // If root (>) or sexpr then create an empty list
+  lval *x = NULL;
+  if (strcmp(t->tag, ">") == 0) { x = lval_sexpr(); }
+  if (strstr(t->tag, "sexpr"))  { x = lval_sexpr(); }
+
+  // Fill this list with any valid expression contained within
+  for (int i = 0; i < t->children_num; i++) {
+    if (strcmp(t->children[i]->contents, "(") == 0) { continue; }
+    if (strcmp(t->children[i]->contents, ")") == 0) { continue; }
+    if (strcmp(t->children[i]->contents, "{") == 0) { continue; }
+    if (strcmp(t->children[i]->contents, "}") == 0) { continue; }
+    if (strcmp(t->children[i]->tag,  "regex") == 0) { continue; }
+    x = lval_add(x, lval_read(t->children[i]));
+  }
+
+  return x;
+}
+
 /* Use the operator string to see which operation to perform */
 
 long eval_op(lval x, char *op, lval y){
